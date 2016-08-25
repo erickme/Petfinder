7a2ec021bd88e5430aa0bcc6e4e4fdd4753ccd6c
Erick Mejia <erick.mejia@gmail.com>
2016-08-24 23:05:02 -0400

    unit testing

---
+++ app/filter/filter.controller.spec.js
@@ -0,0 +1,26 @@
+﻿describe('FilterController', function () {
+    beforeEach(module('petfinder.filter'));
+
+    var $controller;
+
+    beforeEach(inject(function (_$controller) {
+        $controller = _$controller_;
+    }));
+
+    describe('$scope.getBreeds', function () {
+        it('gets breed list for dog and should return a list with a length more than 0', function () {
+            var $scope = {};
+            var controller = $controller('FilterController', { $scope: $scope });
+            $scope.filter = { type: 'dog' };
+            $scope.getBreeds();
+            expect($scope.breeds.length).toBeMoreThan(0);
+        });
+        it('sets filter.type to null and gets breed list and should return a list with a length more than 0', function () {
+            var $scope = {};
+            var controller = $controller('FilterController', { $scope: $scope });
+            $scope.filter = { type: null };
+            $scope.getBreeds();
+            expect($scope.breeds.length).toEqual(0);
+        });
+    });
+});
---
+++ app/results/results.controller.spec.js
@@ -0,0 +1,20 @@
+﻿describe('ResultsController', function () {
+    beforeEach(module('petfinder.results'));
+
+    var $controller;
+
+    beforeEach(inject(function (_$controller) {
+        $controller = _$controller_;
+    }));
+
+    describe('$scope.numberOfPages', function () {
+        it('sets pagesize to 12 and array of perList with 15, expects pages to equal 2', function () {
+            var $scope = {};
+            var controller = $controller('ResultsController', { $scope: $scope });
+            $scope.pageSize = 12;
+            $scope.petList = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+            var pages = $scope.numberOfPages();
+            expect(pages).toEqual(2);
+        });
+    });
+});
